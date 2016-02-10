86f06da9e2aa90350ed19c182cc14b276c1c1900
Matt Lemon <u0575787@utah.edu>
2016-02-09 22:01:36 -0700

    Prototype - Start scene, Main scene

---
+++ life_or_debt/Assets/Scripts/Game_Managers/Game_Manager.cs
@@ -0,0 +1,52 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class Game_Manager : MonoBehaviour
+{
+
+    public enum Game_States
+    {
+        START,
+        MAIN
+    }
+
+    public static Game_Manager instance;
+    public Game_States current_state;
+
+    public bool scene_loaded;
+
+	// Use this for initialization
+	void Awake ()
+    {
+        if (instance == null)
+            instance = this;
+        else if (instance != this)
+            Destroy(gameObject);
+        current_state = Game_States.START;
+        DontDestroyOnLoad(gameObject);
+        scene_loaded = false;
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+	    switch(current_state)
+        {
+            case Game_States.START:
+                if(!scene_loaded)
+                {
+                    scene_loaded = true;
+                    SceneManager.LoadScene("Start_Scene");              
+                }
+                break;
+            case Game_States.MAIN:
+                if(!scene_loaded)
+                {
+                    scene_loaded = true;
+                    SceneManager.LoadScene("Main_Scene");
+                }
+                break;
+        }
+	}
+}
---
+++ life_or_debt/Assets/Scripts/Game_Managers/Game_States/Start_Screen.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Start_Screen : MonoBehaviour
+{
+
+	// Use this for initialization
+	void Start ()
+    {
+	
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+	
+	}
+
+    public void Game_Start()
+    {
+        Game_Manager.instance.current_state = Game_Manager.Game_States.MAIN;
+        Game_Manager.instance.scene_loaded = false;
+    }
+}
