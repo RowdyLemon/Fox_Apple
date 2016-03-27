90a4e5c86e29b14c1bd2931f9b85a289f341b7f1
Matt Lemon <u0575787@utah.edu>
2016-03-27 13:09:57 -0600

    Bars r us

---
+++ life_or_debt/Assets/Scripts/Player/Entertainment_Param.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Entertainment_Param : MonoBehaviour
+{
+    public int amount;
+    public int cost;
+    public int happiness_change;
+}
