1832e23808dd55ccc5ac51ac688f10d6156787fd
Matt Lemon <u0575787@utah.edu>
2016-03-27 12:17:53 -0600

    Bar update

--- life_or_debt/Assets/Scripts/Game_Managers/Game_States/Game.cs
+++ life_or_debt/Assets/Scripts/Game_Managers/Game_States/Game.cs
@@ -12,14 +12,23 @@ public class Game : MonoBehaviour
     public GameObject go_to_work;
     public GameObject go_out;
     public GameObject time;
-    public GameObject happiness;
+
+    // Player bars
+    public GameObject happiness_bar;
+    public GameObject rest_bar;
+    public GameObject job_bar;
+
+    private double bar_full; // player bars full width
 
 	// Use this for initialization
 	void Start ()
     {
         font_init();
         set_top_values();
-	}
+        set_side_values();
+        bar_full = GameObject.Find("happiness_bar_empty").GetComponent<RectTransform>().rect.width;
+        alter_bar_values();
+    }
 	
 	// Update is called once per frame
 	void Update ()
@@ -41,7 +50,7 @@ public class Game : MonoBehaviour
         day_passed(amount);
 
         Game_Manager.instance.Player.Debt -= cost;
-        Game_Manager.instance.Player.Happiness += happiness_change;
+        Game_Manager.instance.Player.Happiness = (Game_Manager.instance.Player.Happiness + happiness_change > 100) ? 100 : Game_Manager.instance.Player.Happiness + happiness_change;
         Game_Manager.instance.Player.Time_Played += amount;
 
     }
@@ -142,5 +151,20 @@ public class Game : MonoBehaviour
     {
         top.GetComponentsInChildren<Text>()[0].text = Game_Manager.instance.Player.Name;
         top.GetComponentsInChildren<Text>()[2].text = Game_Manager.instance.Player.Debt.ToString("N0");
+    }
+
+    private void set_side_values()
+    {
+        side.GetComponentsInChildren<Text>()[4].text = Game_Manager.instance.Player.Player_Job.Name;
+        side.GetComponentsInChildren<Text>()[6].text = Game_Manager.instance.Player.Player_Job.Hourly_Wage + "/hour";
+    }
+
+    private void alter_bar_values()
+    {
+        double happy_bar = bar_full - (bar_full) * ((double)Game_Manager.instance.Player.Happiness / 100.0);
+        Debug.Log(happy_bar);
+        happiness_bar.GetComponent<RectTransform>().pivot = new Vector2(0, 0);
+        //happiness_bar.transform.Translate(new Vector3((float)happy_bar, 0f, 0f));
+        happiness_bar.GetComponent<RectTransform>().sizeDelta = new Vector3(-(float)happy_bar, 0);
     }
 }
