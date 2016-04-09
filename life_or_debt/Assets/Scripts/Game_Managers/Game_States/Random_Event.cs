3195f577566c3ee276c3f42f55163ddf7f120849
DerekHeldtWerle <derekwerle@aol.com>
2016-04-09 13:25:24 -0600

    Lottery now properly works

--- life_or_debt/Assets/Scripts/Game_Managers/Game_States/Random_Event.cs
+++ life_or_debt/Assets/Scripts/Game_Managers/Game_States/Random_Event.cs
@@ -6,7 +6,8 @@ public class Random_Event {
     private string Event_Description;
     private int Debt_Change;
     private int Happiness_Change;
-    private Random_Event[] Event_Array = new Random_Event[3]; 
+    private Random_Event[] Event_Array = new Random_Event[3];
+    private double thriftyness_multiplier;
 
     public Random_Event(string event_description, int debt_change, int happiness_change)
     {
@@ -17,7 +18,7 @@ public class Random_Event {
 
     public Random_Event()
     {
-        double thriftyness_multiplier = Game_Manager.instance.Player.Player_Traits.Thriftyness;
+        thriftyness_multiplier = Game_Manager.instance.Player.Player_Traits.Thriftyness;
         Event_Array[0] = new Random_Event("Your car has broken down, $"+(int)(-500 * thriftyness_multiplier), (int)(-500 * thriftyness_multiplier), -5);
         Event_Array[1] = new Random_Event("Steam Sale. You know the drill $"+(int)(-50 * thriftyness_multiplier), (int)(-50 * thriftyness_multiplier), 0);
         Event_Array[2] = new Random_Event("You've won the lottery! $"+(int)(1000 * thriftyness_multiplier)+"!", (int)(1000 * thriftyness_multiplier), 10);
@@ -41,7 +42,7 @@ public class Random_Event {
         }
         else if (Random_Value == 5)
         {
-            Game_Manager.instance.Player.Debt += Event_Array[2].Debt_Change;
+            Game_Manager.instance.Player.Checking_Account += (int)(1000 * thriftyness_multiplier);
             Game_Manager.instance.Player.Happiness = (Game_Manager.instance.Player.Happiness + Event_Array[2].Happiness_Change > 100) ? 100 : Game_Manager.instance.Player.Happiness + Event_Array[2].Happiness_Change;
             return Event_Array[2].Event_Description;
         }
